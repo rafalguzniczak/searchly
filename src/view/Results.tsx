5b631e8a570d9ef7804891fd3d154256b66231cc
Rafał Guźniczak <guzniczak.rafal@gmail.com>
2021-08-01 21:30:15 +0200

    Render table with results

--- src/view/Results.tsx
+++ src/view/Results.tsx
@@ -4,16 +4,31 @@ import { RepositoriesContext } from "../RepositoriesContextProvider";
 const Results = (): JSX.Element => {
   const repositoriesContext = useContext(RepositoriesContext);
 
-  console.log(repositoriesContext.results);
-
   if (repositoriesContext.results.length === 0) return null;
 
   return (
-    <>
-      {repositoriesContext.results.map((result) => (
-        <div key={result.name}>{result.name} | {result.owner}</div>
-      ))}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Owner</th>
+          <th scope="col">Stars</th>
+          <th scope="col">Created at</th>
+        </tr>
+      </thead>
+      <tbody>
+        {repositoriesContext.results.map((result: RepositoryResult) => (
+          <tr key={`${result.name}${result.owner}`}>
+            <td scope="row" data-label="Name">
+              {result.name}
+            </td>
+            <td data-label="Owner">{result.owner}</td>
+            <td data-label="Stars">{result.stars}</td>
+            <td data-label="Created at">{result.createdAt}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
